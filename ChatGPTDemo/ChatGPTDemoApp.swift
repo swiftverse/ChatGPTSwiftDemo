b22d0c44d2116268b0706d7a4d4b910dac3be5d5
Amit Shrivastava <amitsrivastava115@yahoo.com>
2023-03-29 15:09:09 +0530

    Initial Commit

---
+++ ChatGPTDemo/ChatGPTDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTDemoApp.swift
+//  ChatGPTDemo
+//
+//  Created by Amit Shrivastava on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
